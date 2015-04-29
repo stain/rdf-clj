4e2209100d8f08767d5b3e66636d1cf20357556d
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-04-29 16:47:24 +0100

    commons-rdf-clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject commons-rdf-clj "0.1.0-SNAPSHOT"
+  :description "Clojure wrapper for Apache Commons RDF"
+  :url "http://commonsrdf.incubator.apache.org/"
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.apache.commons/commons-rdf-api "0.1-incubating-SNAPSHOT"]
+                 [org.apache.commons/commons-rdf-simple "0.1.0-incubating-SNAPSHOT"]
+                ])
