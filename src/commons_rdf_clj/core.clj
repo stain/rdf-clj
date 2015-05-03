88e3941e66117efba03a2a87a5cb688104ef3881
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-05-04 00:18:19 +0100

    create-iri and create-literal

--- src/commons_rdf_clj/core.clj
+++ src/commons_rdf_clj/core.clj
@@ -1,11 +1,11 @@
 (ns commons-rdf-clj.core
   (:import
-    (org.apache.commons.rdf.api Graph)
+    (org.apache.commons.rdf.api Graph IRI)
     (org.apache.commons.rdf.simple SimpleRDFTermFactory))
   )
 
 
-(def ^:dynamic *factory* (SimpleRDFTermFactory.))
+(def ^:dynamic *factory* (new SimpleRDFTermFactory))
 
 (defmacro with-factory [factory & body]
   `(binding [*factory* ~factory]
@@ -14,6 +14,21 @@
 (defn create-graph []
   (.createGraph *factory*))
 
+(defn create-iri [iri-string]
+  (.createIRI *factory* iri-string))
 
-(defn graph-size [^Graph g]
-  (.size g))
+(defn create-literal
+  ([^String literal]
+    (.createLiteral *factory* literal))
+  ([^String literal ^IRI type]
+    (.createLiteral *factory* literal type)))
+
+
+(defn create-triple [subject predicate object]
+  (.createTriple *factory* subject predicate object))
+
+(defn add-triple [graph triple]
+  (.add graph triple))
+
+(defn graph-size [^Graph graph]
+  (.size graph))
--- test/commons_rdf_clj/core_test.clj
+++ test/commons_rdf_clj/core_test.clj
@@ -5,6 +5,25 @@
 
 
 (deftest test-create-graph
-  (testing "Creating a graph"
+  (testing "Creating an empty graph"
     (let [g (create-graph)]
       (is (= 0 (graph-size g))))))
+
+(deftest test-create-iri
+  (testing "Creating IRI"
+    (let [iri (create-iri "http://example.com/")]
+      (is (= "http://example.com/" (.getIRIString iri))))))
+
+(deftest test-create-literal
+  (testing "Creating plain literal"
+    (let [lit (create-literal "Hello")]
+      (is (= "Hello" (.getLexicalForm lit)))))
+  (testing "Creating typed literal"
+    (let [xsd_double (create-iri "http://www.w3.org/2001/XMLSchema#double")
+          lit (create-literal "13.37" xsd_double)]
+          (is (= "13.37" (.getLexicalForm lit)))
+          (is (= xsd_double (.getDatatype lit)))))
+  (testing "Creating language literal"
+    (let [lit (create-literal "Hello" "en")]
+      (is (= "Hello" (.getLexicalForm lit)))
+      (is (= "en" (.get (.getLanguageTag lit)))))))
