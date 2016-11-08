a1287d43fc30fcf45b95ae93246ad500c42b4ef3
Stian Soiland-Reyes <stain@apache.org>
2016-11-08 23:59:37 +0000

    Commons RDF 0.3.0, extend protocols

--- project.clj
+++ project.clj
@@ -3,10 +3,13 @@
   :url "http://commonsrdf.incubator.apache.org/"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.commons/commons-rdf-api "0.3.0-incubating"]
                  [org.apache.commons/commons-rdf-simple "0.3.0-incubating"]
+;                 [org.apache.commons/commons-rdf-jena "0.3.0-incubating"]
+;                 [org.apache.commons/commons-rdf-rdf4j "0.3.0-incubating"]
+;                 [org.apache.commons/commons-rdf-jsonld-java "0.3.0-incubating"]
 ;                [potemkin "0.3.13"]
                 ]
-  :repositories { "apache-snapshot" "http://repository.apache.org/snapshots"}
+;  :repositories { "apache-snapshot" "http://repository.apache.org/snapshots"}
               )
