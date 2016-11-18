6b99cc020979a26cecd4956f3717c1d4e9dfeb7d
Stian Soiland-Reyes <stain@apache.org>
2016-11-18 08:53:32 +0000

    namespace-based namespaces

--- src/commons_rdf_clj/ns.clj
+++ src/commons_rdf_clj/ns.clj
@@ -1,27 +1,31 @@
-(ns commons-rdf-clj.n
+(ns rdf.ns
   (:require
     [commons-rdf-clj.protocols :as p]))
+
+(defn- iri [base local]
+  {:iri (str base local)})
+
+(defn rdf-namespace [base & names]
+  (apply hash-map (mapcat #(list %1 (uri base %1)))
+          (map name names)))
 
 
 (defn- ns-rdf- [namespace base & names]
   (let [n (create-ns (symbol namespace))]
-    (doall (map #(intern n (symbol %1) {:iri (str base %1)})
+    (doall (map #(intern n (symbol %1) (iri base %1))
     (map name names)))))
 
 ;
-(defmacro ns-rdf [namespace base & names]
-  `(ns-rdf- ~(symbol namespace) ~base
-    ~@names))
+(defmacro ns-rdf [n base & names]
+  `(ns-rdf- (symbol '~n) ~base ~@names)
+  `(def ~n (rdf-namespace ~base ~@names))
+)
 
 
-;(defn rdf-namespace [base & names]
-;  (apply hash-map (mapcat #(list %1 {:uri (str base (name %1))})
-;          names)))
-
-(def rdf (rdf-namespace "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+(ns-rdf rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
   :langString :HTML :PlainLiteral :XMLLiteral))
 
-(def xsd (rdf-namespace "http://www.w3.org/2001/XMLSchema#"
+(ns-rdf xsd "http://www.w3.org/2001/XMLSchema#"
   :string   :normalizedString
 
   :anyURI
