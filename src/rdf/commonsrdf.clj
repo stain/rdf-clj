c45370cd7bf514a4b8321d7f6363ff5ac59ff6db
Fredrick Eisele <phreed@gmail.com>
2017-10-25 17:59:35 -0500

    condition on type-or-lany g introduced excess arguments

--- src/rdf/commonsrdf.clj
+++ src/rdf/commonsrdf.clj
@@ -12,8 +12,8 @@
   (keyword (last (clojure.string/split
     ; Last part of implementation class package name, e.g.
     ; org.apache.commons.rdf.simple.SimpleRDF -> :simple
-    (.getName (.getPackage (class rdf)))
-     #"\." ))))
+                  (.getName (.getPackage (class rdf)))
+                  #"\."))))
 
 (defn- simpleRDF [] (SimpleRDF.))
 
@@ -28,43 +28,43 @@
 
 (defn- rdf-impls-seq []
   (iterator-seq (.iterator
-    (java.util.ServiceLoader/load RDF))))
+                 (java.util.ServiceLoader/load RDF))))
 
 (defn rdf-impls []
   (apply hash-map (mapcat #(list (key-for-rdf-impl %1) %1)
-    (rdf-impls-seq))))
+                   (rdf-impls-seq))))
 
 (defn rdf-impl
   ([] (first (rdf-impls-seq)))
   ([k]
-    (if (= :simple k) (simpleRDF) ; no need for ServiceLoader and mapping
-      (get (rdf-impls) (keyword k)))))
+   (if (= :simple k) (simpleRDF) ; no need for ServiceLoader and mapping
+     (get (rdf-impls) (keyword k)))))
 
 
 (extend-type Graph
   p/Graph
     (add-triple
       ([g tripl]
-        (.add g (p/triple (simpleRDF) tripl)
+       (.add g (p/triple (simpleRDF) tripl)
         g)) ; Return same instance
       ([g subj pred obj]
-        (.add g (p/triple (simpleRDF) subj pred obj)
+       (.add g (p/triple (simpleRDF) subj pred obj)
         g)))
     (remove-triple
       ([g tripl]
-        (.remove g (p/triple (simpleRDF) tripl))
-        g)
+       (.remove g (p/triple (simpleRDF) tripl))
+       g)
       ([g subj pred obj]
-        (.remove g (termPattern subj) (termPattern pred) (termPattern obj))
-        g))
+       (.remove g (termPattern subj) (termPattern pred) (termPattern obj))
+       g))
     (contains-triple?
       ([g tripl]
-        (.contains g (p/triple (simpleRDF) tripl)))
+       (.contains g (p/triple (simpleRDF) tripl)))
       ([g subj pred obj]
-        (.contains g (termPattern subj) (termPattern pred) (termPattern obj))))
+       (.contains g (termPattern subj) (termPattern pred) (termPattern obj))))
     (triple-count [g]
-      (.size g))
-)
+      (.size g)))
+
 
 
 (extend-type Triple
@@ -79,31 +79,31 @@
     (graph
         ([f] (.createGraph f))
         ([f g] (if-instance Graph g
-          (reduce p/add-triple (p/graph f) g))))
+                (reduce p/add-triple (p/graph f) g))))
     (iri [f iri] (if-instance IRI iri (.createIRI f (p/iri-str iri))))
     (literal
       ([f lit]
-        (if-instance Literal lit) (.createLiteral f (p/literal-str lit)))
+       (if-instance Literal lit) (.createLiteral f (p/literal-str lit)))
       ([f lit type-or-lang]
-        (.createLiteral f (str lit)
-          (if (p/iri? type-or-lang)
-            (p/iri f type-or-lang)) ; must be type
-            (name type-or-lang))); must be non-nil lang
+       (.createLiteral f (str lit)
+         (if (p/iri? type-or-lang)
+           (p/iri f type-or-lang) ; must be type
+           (name type-or-lang)))); must be non-nil lang
       ([f lit type lang]
-        (if (nil? lang)
-          (.createLiteral f (str lit) (p/iri f type))
-          (.createLiteral f (str lit) (p/iri f type) (name lang)))))
+       (if (nil? lang)
+         (.createLiteral f (str lit) (p/iri f type))
+         (.createLiteral f (str lit) (p/iri f type) (name lang)))))
 
     (blanknode
       ([f]
-        (.createBlankNode f))
+       (.createBlankNode f))
       ([f name]
-        (.createBlankNode f (str name))))
+       (.createBlankNode f (str name))))
     (triple
       ([f t]
-        (if-instance Triple t (.createTriple f (p/subject t) (p/predicate t) (p/object t))))
-      ([f subj pred obj] (.createTriple f subj pred obj))) ;; TODO: Type conversion
-)
+       (if-instance Triple t (.createTriple f (p/subject t) (p/predicate t) (p/object t))))
+      ([f subj pred obj] (.createTriple f subj pred obj)))) ;; TODO: Type conversion
+
 
 
 (extend-type RDFTerm p/Term
